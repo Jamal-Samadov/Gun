caf029b3fe77427f9b3319339be4766cfccc9a7c
Jamaltscode <jamalts@code.edu.az>
2022-05-07 13:43:04 +0400

    gun creating

---
+++ Gun Shoot/LabTraining/Gun.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabTraining
+{
+    internal class Gun
+    {
+        private int _totalBullletSize;
+        public int TotalBulletSize
+        {
+            get 
+            {
+                return _totalBullletSize;
+            }
+            private set
+            {
+                if(value >= 0)
+                {
+                    _totalBullletSize = value;
+                }
+            }
+        }
+
+        private int _shoot;
+        public int Shoot
+        {
+            get
+            {
+                return _shoot;
+            }
+            set
+            {
+                int select = 30;
+                int count = 0;
+                for(int i = 0; i < select; i--)
+                {
+                    if (i > 0)
+                    {
+                        count++;
+
+                    }
+                    else if(i == 0)
+                    {
+                        Console.WriteLine("Do reload");
+                    }
+                }
+            }
+        }
+
+
+        public int _fullShoot;
+
+        public int FullShoot
+        {
+            get
+            {
+                return  _fullShoot;
+            }
+            set
+            {
+                int i;
+
+                for (i = 30; i >= 0; i--)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            }
+        }
+        private string _gunShoot;
+        public string Gunshoot
+        {
+            get
+            {
+                return _gunShoot;
+            }
+            set
+            {
+                string name = "My gun is M416. I have 30 bullet";
+            }
+        }
+        
+
+
+
+
+    }
+}
---
+++ Gun Shoot/LabTraining/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace LabTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear Friend. Welcome to my World \n");
+            Console.WriteLine("I have m416 gun \n");
+            Console.WriteLine("Do you want to my gun?");
+            Console.WriteLine("Okey let's go \n");
+            Console.WriteLine("********");
+
+
+            Gun gun = new Gun();
+
+            Console.WriteLine("1-Reload");
+            Console.WriteLine("2-Shoot");
+            Console.WriteLine("3-Full shoot");
+            Console.WriteLine("4-Gun Information");
+            Console.WriteLine();
+
+            
+
+            bool control = true;
+
+            while (control)
+            {
+                int select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+
+
+                    case 1:                        
+                        Console.WriteLine("1-Reload \n");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("2-Shoot \n");
+
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("3-Full shoot \n");
+
+                        break;
+
+                    case 4:
+
+                        Console.WriteLine("4-Gun Information \n");
+
+                        control = false;
+                        break;
+
+                }
+
+                static void GetInfo()
+                {
+                    Console.WriteLine("1-Circle");
+                    Console.WriteLine("2-Triangle");
+                    Console.WriteLine("3-Rectangle");
+                    Console.WriteLine("4-Exit");
+                    Console.WriteLine();
+
+                }
+
+            }
+        }
+
+    }
+}
